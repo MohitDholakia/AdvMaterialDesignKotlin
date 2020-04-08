eba92fc47c2486c82b20e0529fa1c74dcc6eba99
Mohit Dholakia <mohit.dholakia@solutionanalysts.com>
2020-04-08 19:13:36 +0530

    [feature] - Bottom App Bar added [feature] - BackDrop added

--- app/src/main/java/com/md/advmaterialdesign/BackdropActivity.kt
+++ app/src/main/java/com/md/advmaterialdesign/BackdropActivity.kt
@@ -2,11 +2,28 @@ package com.md.advmaterialdesign
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.animation.LinearInterpolator
+import android.widget.Toolbar
+import com.evolve.backdroplibrary.BackdropContainer
+
+
+
+
 
 class BackdropActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_backdrop)
+
+       val  backdropContainer = findViewById(R.id.backdropcontainer) as BackdropContainer
+       val  toolbar = findViewById(R.id.toolbar) as androidx.appcompat.widget.Toolbar
+        val height = this.resources.getDimensionPixelSize(R.dimen.sneek_height)
+
+        backdropContainer.attachToolbar(toolbar)
+            .dropInterpolator(LinearInterpolator())
+            .dropHeight(height)
+            .build()
+
     }
 }
--- app/src/main/java/com/md/advmaterialdesign/BottomAppBarActivity.kt
+++ app/src/main/java/com/md/advmaterialdesign/BottomAppBarActivity.kt
@@ -8,5 +8,11 @@ class BottomAppBarActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_bottom_app_bar)
+
+        // Set compileSdkVersion, and targetSdkVersion to the latest API version targetting Android P which is 28
+
+        //android:theme="@style/Widget.MaterialComponents.BottomAppBar"
+
+
     }
 }
